cad1306149aadbf81e22a6f40101b299e7d80227
Alex Odorico <alex.odorico@morleynet.com>
2019-08-12 09:11:51 -0400

    Added error handling

--- src/auth.js
+++ src/auth.js
@@ -13,14 +13,14 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const decoded = await jwt.verify(token, process.env.SECRET);
-    const text = "SELECT * FROM users WHERE id = $1";
-    const { rows } = await db.query(text, [decoded.userId]);
+    const query = "SELECT * FROM users WHERE username = $1";
+    const { rows } = await db.query(query, [decoded.userId]);
 
     if (!rows[0]) {
       return res.status(400).json({ error: "The token is expired" });
     }
 
-    req.user = { id: decoded.userId };
+    req.userId = { id: decoded.userId };
     next();
   } catch (error) {
     return res.status(400).json(error);
@@ -31,7 +31,13 @@ const login = async (req, res) => {
   const query = "SELECT * FROM users WHERE username = $1 LIMIT 1";
   const { username, password } = req.body;
 
-  const { rows } = await db.query(query, [username]);
+  const { rows } = await db.query(query, [username], error => {
+    if (error) {
+      return res.status(400).json({
+        error: "Oops! Something went wrong. Please try again in a sec."
+      });
+    }
+  });
 
   if (rows.length) {
     const isValid = await bcrypt.compare(password, rows[0].password);
@@ -39,7 +45,7 @@ const login = async (req, res) => {
     if (isValid) {
       const token = jwt.sign(
         {
-          userId: rows[0].id
+          userId: rows[0].userId
         },
         process.env.SECRET,
         {
@@ -50,25 +56,29 @@ const login = async (req, res) => {
       return res.status(200).json({ token });
     }
 
-    return res.status(401).json({ error: "incorrect username or password" });
+    return res
+      .status(401)
+      .json({ error: "Please double check your password and username" });
   }
 
-  return res.status(401).json({ error: "incorrect username or password" });
+  return res
+    .status(401)
+    .json({ error: "Please double check your password and username" });
 };
 
 const createUser = async (req, res) => {
   const { email, username, password } = req.body;
   const hash = await bcrypt.hash(password, 10);
   const query =
-    "INSERT INTO users (email, username, password) VALUES ($1, $2 $3)";
+    "INSERT INTO users (email, username, password) VALUES ($1, $2, $3)";
 
-  db.query(query, [email, username, hash], error => {
-    if (error) {
-      throw error;
-    }
+  try {
+    db.query(query, [email, username, hash]);
+  } catch (error) {
+    res.status(401).json({ error: "Oops! All fields are required!" });
+  }
 
-    return res.status(201).send(`User added`);
-  });
+  return res.status(201).send("User added");
 };
 
 export default {
